5d1ef22a2ce731d69e46603b3bbf72e978ebd836
Luke Keltner <LKelt@hawken.edu>
2017-10-19 10:16:13 -0400

    more spinner things

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ var predict = function(cb)
 		data.push(0)
 	}
 
+	py.stdin.write(JSON.stringify(data));
+	py.stdin.end();
+
 	dataString = '';
 	/*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), we want to convert that received data into a string and append it to the overall dataString.*/
 	py.stdout.on('data', function(data)
@@ -51,8 +54,6 @@ var predict = function(cb)
 	});
 
 	/*We have to stringify the data first otherwise our python process wont recognize it*/
-	py.stdin.write(JSON.stringify(data));
-	py.stdin.end();
 }
 
 app.listen(3000)
